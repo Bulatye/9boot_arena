8c235b8a1a849ade479b82158dd9edc0c1a1fc21
Dmitrii Popov <hiderline@yandex.ru>
2024-06-09 15:54:46 +0700

    useless commit

---
+++ things_set.py
@@ -0,0 +1,50 @@
+thing_type = [
+    {
+        'name': 'Helmet',
+        'protection_percent': 0.025,
+        'attack': 5,
+        'helth': 10,
+    },
+    {
+        'name': 'Armor',
+        'protection_percent': 0.05,
+        'attack': 3,
+        'helth': 50,
+    },
+    {
+        'name': 'Ring',
+        'protection_percent': 0.005,
+        'attack': 1,
+        'helth': 1,
+    },
+    {
+        'name': 'Gloves',
+        'protection_percent': 0.015,
+        'attack': 5,
+        'helth': 5,
+    },
+    {
+        'name': 'Sword',
+        'protection_percent': 0.025,
+        'attack': 15,
+        'helth': 0,
+    },
+    {
+        'name': 'Wand',
+        'protection_percent': 0.005,
+        'attack': 10,
+        'helth': 3,
+    },
+    {
+        'name': 'Shield',
+        'protection_percent': 0.05,
+        'attack': 10,
+        'helth': 25,
+    },
+    {
+        'name': 'Knife',
+        'protection_percent': 0.001,
+        'attack': 15,
+        'helth': 0,
+    },
+]
